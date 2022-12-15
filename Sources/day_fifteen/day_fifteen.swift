62aaef782fc5aa234b0ff97e23c7b81952ae7e57
Grayson Smith <d.grayson.smith@gmail.com>
2022-12-14 21:01:29 -0800

    Initial Commit

---
+++ Sources/day_fifteen/day_fifteen.swift
@@ -0,0 +1,6 @@
+public struct day_fifteen {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/day_fifteenTests/day_fifteenTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import day_fifteen
+
+final class day_fifteenTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(day_fifteen().text, "Hello, World!")
+    }
+}
